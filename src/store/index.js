e54fd571d1557b5f97dae932846588b4f9c1d0c9
M. Görkem <63784397+mgb17@users.noreply.github.com>
2021-06-16 12:18:00 +0200

    profile view + update user settings

--- src/store/index.js
+++ src/store/index.js
@@ -42,6 +42,15 @@ export default new Vuex.Store({
       state.profileInitials =
         state.profileFirstName.match(/(\b\S)?/g).join("") + state.profileLastName.match(/(\b\S)?/g).join("");
     },
+    changeFirstName(state, payload) {
+      state.profileFirstName = payload
+    },
+    changeLastName(state, payload) {
+      state.profileLastName = payload
+    },
+    changeUsername(state, payload) {
+      state.profileUsername = payload
+    },
   },
   actions: {
     async getUser({commit}) {
@@ -50,6 +59,15 @@ export default new Vuex.Store({
       commit("setProfile", dbResults);
       commit("setProfileInitials");
       console.log(dbResults)
+    },
+    async updateUserSettings({commit, state}) {
+      const dataBase= await db.collection('users').doc(state.profileId);
+      await dataBase.update({
+        firstName: state.profileFirstName,
+        lastName: state.profileLastName,
+        username: state.profileUsername,
+      });
+      commit("setProfileInitials");
     }
   },
   modules: {
